4da2db46acd901c2906cabf969bfbe1c93a25deb
Arata <arata04962@gmail.com>
2026-01-07 04:53:03 +0900

    chore: find llvm-config.exe on windows

--- build.rs
+++ build.rs
@@ -79,8 +79,14 @@ fn run() -> Result<(), Box<dyn Error>> {
 }
 
 fn locate_llvm_config() -> Result<PathBuf, Box<dyn Error>> {
+    let bin_name = if cfg!(target_os = "windows") {
+        "llvm-config.exe"
+    } else {
+        "llvm-config"
+    };
+
     if let Ok(prefix_path) = env::var(format!("TABLEGEN_{LLVM_MAJOR_VERSION}0_PREFIX")) {
-        let llvm_config = PathBuf::from(prefix_path).join("bin").join("llvm-config");
+        let llvm_config = PathBuf::from(prefix_path).join("bin").join(&bin_name);
         if llvm_config.exists() {
             return Ok(llvm_config);
         }
@@ -89,21 +95,21 @@ fn locate_llvm_config() -> Result<PathBuf, Box<dyn Error>> {
     // Homebrew (macOS)
     if cfg!(target_os = "macos") {
         if let Some(prefix) = homebrew_prefix(&format!("llvm@{}", LLVM_MAJOR_VERSION)) {
-            let llvm_config = PathBuf::from(prefix).join("bin").join("llvm-config");
+            let llvm_config = PathBuf::from(prefix).join("bin").join(&bin_name);
             if llvm_config.exists() {
                 return Ok(llvm_config);
             }
         }
 
         if let Some(prefix) = homebrew_prefix("llvm") {
-            let llvm_config = PathBuf::from(prefix).join("bin").join("llvm-config");
+            let llvm_config = PathBuf::from(prefix).join("bin").join(&bin_name);
             if llvm_config.exists() {
                 return Ok(llvm_config);
             }
         }
     }
 
-    Ok(PathBuf::from("llvm-config"))
+    Ok(PathBuf::from(bin_name))
 }
 
 fn homebrew_prefix(name: &str) -> Option<String> {
